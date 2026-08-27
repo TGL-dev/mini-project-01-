b2487bba2a3563e0cf98db98349f3284b333829f
Taghiloo <l.tgl@vatanmail.ir>
2026-08-27 20:57:58 +0330

    code edited by puting each part in diffrent functions

--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from pathlib import Path
+import joblib
 from sklearn.model_selection import cross_validate,GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import (
@@ -30,27 +31,108 @@ def initialize_data(testSize,randomState):
     X_train,X_test,y_train,y_test = dataSplite(X,y,testSize,randomState)
     return X_train,X_test,y_train,y_test
 
+def initialize_model(randomState):
+    LogisticR = Pipeline([
+        ("scaler", StandardScaler()),
+     ("model", LogisticRegression(max_iter=10000))
+     ])
+    Knn = Pipeline([
+    ("KNNClasifier",StandardScaler()),
+    ("model",KNeighborsClassifier(n_neighbors=5))])
+    DecisionTree = DecisionTreeClassifier(random_state = randomState)
+    models = {
+    "Logistic Regression": LogisticR,
+    "KNN": Knn,
+    "Decision Tree": DecisionTree
+    }
+    return models
+
+def CrossValidation(X_train,y_train,skf,models,scorings):
+    Report_results = {'0':['Model'],
+                    '1' :['accuracy'] ,
+                    '2' :['Mean Precision'],
+                    '3' :['Mean Recall'], 
+                    '4' :['Mean F1'] ,
+                    '5' :['Mean roc_auc'],
+                    '6' :['Mean average_precision'],
+                    '7' :['Mean balanced_accuracy']}
+    metrics = ['Mean accuracy','Mean Precision','Mean Recall','Mean F1','Mean roc_auc','Mean average_precision','Mean balanced_accuracy']
+    for name, model in models.items():
+        Cross_validation_Result = cross_validate(
+            model,
+            X_train,
+            y_train,
+            cv = skf,
+            scoring = scorings,
+            return_train_score=True
+        )
+
+        Report_results['0'].append(name)
+        i = 1
+        for metric in scorings.key():
+            mean_score = Cross_validation_Result[f'test_{metric}'].mean()
+            Report_results[str(i)].append(mean_score)
+            i = i+1
+
+    Report_results = pd.DataFrame(Report_results)
+    # print(Report_results)
+    return Report_results
+
+def GridSearch(X_train,y_train,skf,models,scorings):
+    grid_results = {}
+    all_results = []
+    initialize_model(randomState)
+    modelss = {
+    "KNN": models['kNN'],
+    "Decision Tree": models['Decision Tree']
+    }
+    for name, model in modelss.items():
+        grid = GridSearchCV(
+            estimator=model,
+            param_grid=param_grids[name],
+            cv=skf,
+            scoring=scorings,
+            n_jobs=-1,
+            refit='f1',  
+            return_train_score=True
+        )
+        grid.fit(X_train, y_train)
+        grid_results[name] = grid
+        results = pd.DataFrame(grid.cv_results_)
+        results["model"] = name
+        all_results.append(results)
+        print(name)
+        print("Best Parameters:")
+        print(grid.best_params_)
+        print("Best CV F1:")
+        print(grid.best_score_)
+
+    all_results_df = pd.concat(
+        all_results,
+        ignore_index=True
+    )
+    report = all_results_df[
+        [
+            "model",
+            "params",
+            "mean_train_precision",
+            "mean_test_precision",
+            "mean_train_recall",
+            "mean_test_recall",
+            "mean_train_f1",
+            "mean_test_f1"
+        ]
+    ].copy()
+    return report,grid_results
+
 testSize = 0.2
 randomState = 42
 
 X_train,X_test,y_train,y_test = initialize_data(testSize,randomState)
-LogisticR = Pipeline([
-        ("scaler", StandardScaler()),
-     ("model", LogisticRegression(max_iter=10000))
-     ])
-Knn = Pipeline([
-    ("KNNClasifier",StandardScaler()),
-    ("model",KNeighborsClassifier(n_neighbors=5))])
-DecisionTree = DecisionTreeClassifier(random_state = randomState)
-models = {
-    "Logistic Regression": LogisticR,
-    "KNN": Knn,
-    "Decision Tree": DecisionTree
-}
 
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-scoring =  {
+scorings =  {
             "accuracy" : "accuracy",
             "precision" : "precision",
             "recall" : "recall",
@@ -60,40 +142,13 @@ scoring =  {
             "balanced_accuracy": "balanced_accuracy"
         }
 metrics = ['Mean accuracy','Mean Precision','Mean Recall','Mean F1','Mean roc_auc','Mean average_precision','Mean balanced_accuracy']
-Report_results = {'0':['Model'],
-                    '1' :['accuracy'] ,
-                    '2' :['Mean Precision'],
-                    '3' :['Mean Recall'], 
-                    '4' :['Mean F1'] ,
-                    '5' :['Mean roc_auc'],
-                    '6' :['Mean average_precision'],
-                    '7' :['Mean balanced_accuracy']}
-for name, model in models.items():
-    Cross_validation_Result = cross_validate(
-        model,
-        X_train,
-        y_train,
-        cv = skf,
-        scoring = scoring,
-        return_train_score=True
-    )
 
-    Report_results['0'].append(name)
-    i = 1
-    for metric in scoring.keys():
-        mean_score = Cross_validation_Result[f'test_{metric}'].mean()
-        Report_results[str(i)].append(mean_score)
-        i = i+1
-
-Report_results = pd.DataFrame(Report_results)
+models = initialize_model(randomState)
+Report_results = CrossValidation(X_train,y_train,skf,models,scorings)
 print(Report_results)
 
  ####### Gride Search #######
 
-models = {
-    "KNN": Knn,
-    "Decision Tree": DecisionTree
-}
 param_grids = {
     "KNN": {
         'model__n_neighbors': [1, 5 ,20]
@@ -102,50 +157,26 @@ param_grids = {
         'max_depth': [2, 5, 10, None]
     }
 }
-grid_results = {}
-all_results = []
-for name, model in models.items():
-    grid = GridSearchCV(
-        estimator=model,
-        param_grid=param_grids[name],
-        cv=skf,
-        scoring=scoring,
-        n_jobs=-1,
-        refit='f1',  
-        return_train_score=True
-    )
-    grid.fit(X_train, y_train)
-    grid_results[name] = grid
-    grid_results[name] = grid
-    results = pd.DataFrame(grid.cv_results_)
-    results["model"] = name
-    all_results.append(results)
-    print(name)
-    print("Best Parameters:")
-    print(grid.best_params_)
-    print("Best CV F1:")
-    print(grid.best_score_)
-all_results_df = pd.concat(
-    all_results,
-    ignore_index=True
-)
-report = all_results_df[
-    [
-        "model",
-        "params",
-        "mean_train_precision",
-        "mean_test_precision",
-        "mean_train_recall",
-        "mean_test_recall",
-        "mean_train_f1",
-        "mean_test_f1"
-    ]
-].copy()
+
+report , grid_results = GridSearch(X_train,y_train,skf,models,scorings)
 print("Report result :")
 print(report)
 
+best_model = grid_results['KNN'].best_estimator_
+print('Best hyperparameter for KNN model is ')
+print(best_model)
 
-    
+best_model = grid_results['Decision Tree'].best_estimator_
+print('Best hyperparameter for KNN model is ')
+print(best_model)
+
+
+# joblib.dump(
+#     best_model,
+#     "models/best_model.pkl"
+# )   
+
+
 
 
 
